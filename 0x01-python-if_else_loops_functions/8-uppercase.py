bf4b05a73dd82744e13444446452395fce7f76c2
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-05-10 00:47:29 +0100

    fix: using .format()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
 def uppercase(str):
-    # print("".join(map(lambda c:
-    #                   chr(ord(c) - 32) if 'a' <= c <= 'z' else c, str)))
-    print(str.translate(str.maketrans(
-        "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+    print("".join(map(lambda c: "{}".format(
+        chr(ord(c) - 32)) if 'a' <= c <= 'z' else c, str)))

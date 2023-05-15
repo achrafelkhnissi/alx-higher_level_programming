a6f014fe500afd7d959111999ac5def413bdd260
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-05-15 10:00:32 +0100

    fix: betty

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,17 +6,19 @@
  *
  * Return: void
  */
-void print_python_list_info(PyObject *p) {
-  Py_ssize_t size = PyList_Size(p);
-  PyObject **elements = PyList_GetItems(p);
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	PyObject **elements = PyList_GetItems(p);
 
-  printf("[*] Size of the Python List = %zd\n", size);
-  printf("[*] Allocated = %zd\n", PyList_GET_SIZE(p));
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", PyList_GET_SIZE(p));
+	for (Py_ssize_t i = 0; i < size; i++)
+	{
+		PyObject *element = elements[i];
 
-  for (Py_ssize_t i = 0; i < size; i++) {
-    PyObject *element = elements[i];
-    printf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
-  }
+		rintf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
+	}
 
-  Py_DECREF(elements);
+	Py_DECREF(elements);
 }

762ad5388c17f02af6c1e2eac6bf64a33a8e9424
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-05-17 19:46:12 +0100

    test

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,8 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - prints some basic info about Python lists

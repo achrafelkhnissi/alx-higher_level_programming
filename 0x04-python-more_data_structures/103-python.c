90fd4ad0df67fd65dab2fb20215fde816a01e5c9
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-05-17 05:24:19 +0100

    add: task 16

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,63 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @p: pointer to PyObject
+ */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t i, size, alloc;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    alloc = ((PyListObject *)p)->allocated;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", alloc);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+        if (PyBytes_Check(item))
+            print_python_bytes(item);
+    }
+}
+
+/**
+ * print_python_bytes - prints some basic info about Python bytes
+ * @p: pointer to PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t i, size;
+    char *string;
+
+    printf("[.] bytes object info\n");
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = ((PyVarObject *)p)->ob_size;
+    string = ((PyBytesObject *)p)->ob_sval;
+
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", string);
+
+    if (size < 10)
+        printf("  first %ld bytes:", size + 1);
+    else
+        printf("  first 10 bytes:");
+
+    for (i = 0; i <= size && i < 10; i++)
+        printf(" %02hhx", string[i]);
+    printf("\n");
+}

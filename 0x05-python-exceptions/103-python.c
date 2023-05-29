a657bb647e8d3df2bf90499501ad0c08dbabf403
Achraf EL Khnissi <40025388+achrafelkhnissi@users.noreply.github.com>
2023-05-29 22:16:15 +0100

    add: task 10

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,82 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @p: pointer to PyObject
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i, size, alloc;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+		else if (PyFloat_Check(item))
+			print_python_float(item);
+	}
+}
+
+/**
+ * print_python_bytes - prints some basic info about Python bytes
+ * @p: pointer to PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t i, size;
+	char *str;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = ((PyVarObject *)p)->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+	if (size < 10)
+		printf("  first %ld bytes:", size + 1);
+	else
+		printf("  first 10 bytes:");
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", str[i]);
+	printf("\n");
+}
+
+/**
+ * print_python_float - prints some basic info about Python float
+ * @p: pointer to PyObject
+ */
+void print_python_float(PyObject *p)
+{
+	char *str;
+	double d;
+
+	printf("[.] float object info\n");
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	d = ((PyFloatObject *)p)->ob_fval;
+	str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", str);
+	free(str);
+}

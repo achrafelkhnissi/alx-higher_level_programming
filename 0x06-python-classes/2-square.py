baec60c1ac7f5e652afcaa0fd7ff20273cdd428d
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:42:35 +0100

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-
-"""This module defines a Square class"""
-
-
 class Square:
-    """Square class with a private instance attribute"""
+    """Defines a square"""
     def __init__(self, size=0):
-        """Initializes the data"""
+        """Initialises the data"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size

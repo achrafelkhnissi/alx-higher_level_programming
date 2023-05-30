4c402a393a0fa784674f836af0da0349b747719a
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:39:24 +0100

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,8 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data"""
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
-        else:
-            self.__size = size

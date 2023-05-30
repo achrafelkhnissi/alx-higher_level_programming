24b87849a00f77555993997a5c34543d8a294062
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:44:59 +0100

    test: chaning != with !isinstance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+
+"""This module defines a Square class"""
+
+
 class Square:
-    """Defines a square"""
+    """
+    Class that defines a square
+
+    Attributes:
+        size (int): size of the square
+    """
     def __init__(self, size=0):
-        """Initialises the data"""
-        if type(size) != int:
+        """
+        Init method is a constructor for Square class
+        Args:
+            size: size of the square
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >=0")
         self.__size = size

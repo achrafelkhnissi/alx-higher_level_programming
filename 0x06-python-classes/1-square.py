e35a402e8cb808797f71bbe76e74dcf22d773b69
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:34:25 +0100

    add: task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Square class with a private instance attribute"""
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size

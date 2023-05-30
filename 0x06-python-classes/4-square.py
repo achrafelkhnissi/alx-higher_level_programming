5ba97631267abbf9c1d7ee152fcac8b558776c73
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:57:10 +0100

    add: task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,6 +10,7 @@ class Square:
     Attributes:
         size (int): size of the square
     """
+
     def __init__(self, size=0):
         """
         Init method is a constructor for Square class
@@ -24,6 +25,28 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def size(self):
+        """
+        Public instance method that returns the current square size
+        """
+        return self.__size
+
+    def size(self, value):
+        """
+        Public instance method that returns the current square size
+        Args:
+            value: size of the square
+
+        Returns:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """

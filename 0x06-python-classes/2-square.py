9170cf96db28e27c9222be2b1be021f1a9fc0e27
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-30 13:40:15 +0100

    test: removing

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     def __init__(self, size=0):
         """Initializes the data"""
         self.__size = size
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")

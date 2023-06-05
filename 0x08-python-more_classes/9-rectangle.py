74301933f53b4db817f38c9b3e30b38b63232218
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-05 22:53:42 +0000

    fix: line too long

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -103,7 +103,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        return "\n".join([self.print_symbol * self.__width for _ in range(self.__height)])
+        return "\n".join([self.print_symbol *
+                          self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """ Method that returns a string representation of the rectangle """
@@ -141,7 +142,8 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ Method that returns a new Rectangle instance with width == height == size
+        """ Method that returns a new Rectangle instance with
+        width == height == size
 
         Args:
             size: size of the new rectangle

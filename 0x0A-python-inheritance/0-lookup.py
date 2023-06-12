eced8a3fdaf61d485acfd0354e5ee393b61eb929
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-12 11:54:35 +0100

    fix: coding style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Module for lookup function
+"""
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return dir(obj)
-
-if __name__ == "__main__":
-    class MyClass1(object):
-        pass
-
-    class MyClass2(object):
-        my_attr1 = 3
-        def my_meth(self):
-            pass
-
-    print(lookup(MyClass1))
-    print(lookup(MyClass2))
-    print(lookup(int))

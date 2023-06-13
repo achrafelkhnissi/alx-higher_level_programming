cb074fbefb056925b34337c65f6319620be19b05
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-13 10:44:24 +0100

    Edit: moving imports outside of the funciton to make it a pure function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,11 @@ Module for add_item method.
 import sys
 
 
+load_from_json_file = __import__('6-load_from_json_file') \
+    .load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
 def add_item(args):
     """
     adds all arguments to a Python list, and then save them to a file
@@ -17,17 +22,15 @@ def add_item(args):
         args {list} -- [list of arguments]
     """
 
-    load_from_json_file = __import__('6-load_from_json_file')\
-        .load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    filename = "add_item.json"
 
     try:
-        my_list = load_from_json_file("add_item.json")
+        my_list = load_from_json_file(filename)
     except FileNotFoundError:
         my_list = []
     for i in range(1, len(args)):
         my_list.append(args[i])
-    save_to_json_file(my_list, "add_item.json")
+    save_to_json_file(my_list, filename)
 
 
 if __name__ == "__main__":

91904332f27c8d8c4c8c767632fe90b7cfafef69
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-06-13 10:35:56 +0100

    mv two import to the top

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -16,13 +16,15 @@ def add_item(args):
     Arguments:
         args {list} -- [list of arguments]
     """
+
+    load_from_json_file = __import__('6-load_from_json_file')\
+        .load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
     try:
-        load_from_json_file = __import__('6-load_from_json_file')\
-            .load_from_json_file
         my_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
         my_list = []
     for arg in args:
         my_list.append(arg)
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     save_to_json_file(my_list, "add_item.json")
